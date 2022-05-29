cc23b3f6e190be9eb90686a028f91cc0ddb4aab6
Jonathan Armentor <jarmentor@me.com>
2022-05-29 09:11:06 -0700

    add command to backup an arbitrary environment with notification and description options

---
+++ commands/backup.js
@@ -0,0 +1,30 @@
+import fetch from 'node-fetch'
+import { getAllSites, getAuthorization } from '../util.js'
+
+export default async function backup(environment, options) {
+    const sites = await getAllSites();
+
+    const targetEnv = sites.filter(site => site.name == environment);
+    if (targetEnv.length == 0) {
+        return console.log(`No environment with name ${environment} found.`);
+    }
+
+    if (targetEnv.length > 1) {
+        return console.log(`Multiple environments found for name: ${environment}`, targetEnv);
+    }
+
+    const request_body = {
+        description: options.message,
+        notification_emails: options.notificationEmails
+    }
+
+    const res = await fetch(`https://api.wpengineapi.com/v1/installs/${targetEnv[0].id}/backups`, {
+        method: 'POST',
+        body: JSON.stringify(request_body),
+        headers: { Authorization: getAuthorization(), 'Content-Type': 'application/json' }
+    })
+
+    const json = await res.json();
+
+    return console.log(json)
+}
