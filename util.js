25dacc3ee2f0db32916dc7ea72c232bb9bab6aae
Jonathan Armentor <jarmentor@me.com>
2022-05-29 08:09:00 -0700

    centralize some functionality in a util file

---
+++ util.js
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch'
+
+export function getAuthorization() {
+    const WPENGINE_PASSWORD = process.env.WPENGINE_PASSWORD
+    const WPENGINE_USER_ID = process.env.WPENGINE_USER_ID
+    const auth_string = Buffer.from(
+        `${WPENGINE_USER_ID}:${WPENGINE_PASSWORD}`
+    ).toString('base64')
+    return `Basic ${auth_string}`
+}
+
+async function getSites(url = 'https://api.wpengineapi.com/v1/installs') {
+    const res = await fetch(url, {
+        method: 'GET',
+        headers: { Authorization: getAuthorization() },
+    })
+    return await res.json()
+}
+
+export async function getAllSites() {
+    let res = await getSites()
+    const sites = [...res.results]
+    while (res.next) {
+        res = await getSites(res.next)
+        sites.push(...res.results)
+    }
+
+    return sites
+}
